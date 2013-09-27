32a720aac8c094b4596e4bda7678d1ba42032ee2
jy910.yun <jy910.yun@samsung.com>
2013-09-27 15:14:01 +0900

    add device_ops structure for improving feedback structure
    feedback code will be divided into 2 parts by device type
    
    Change-Id: I94e46c234997218371c91dfca1aeb91ba18c3537
    Signed-off-by: jy910.yun <jy910.yun@samsung.com>

---
+++ include/devices.h
@@ -0,0 +1,49 @@
+/*
+ * libfeedback
+
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __DEVICES_H__
+#define __DEVICES_H__
+
+struct device_ops {
+	int type;
+	void (*init) (void);
+	void (*exit) (void);
+	int (*play) (int);
+	int (*get_path) (int, char *, unsigned int);
+	int (*set_path) (int, char *);
+};
+
+void devices_init(void);
+void devices_exit(void);
+
+#define DEVICE_OPS_REGISTER(dev)	\
+static void __CONSTRUCTOR__ module_init(void)	\
+{	\
+	add_device(dev);	\
+}	\
+static void __DESTRUCTOR__ module_exit(void)	\
+{	\
+	remove_device(dev);	\
+}
+
+void add_device(const struct device_ops *dev);
+void remove_device(const struct device_ops *dev);
+const struct device_ops *find_device(int type);
+
+#endif
---
+++ src/devices.c
@@ -0,0 +1,83 @@
+/*
+ * deviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <stdio.h>
+#include <glib.h>
+
+#include "devices.h"
+#include "feedback-str.h"
+#include "feedback-log.h"
+
+#define DD_LIST_PREPEND(a, b)		\
+	a = g_list_prepend(a, b)
+#define DD_LIST_APPEND(a, b)		\
+	a = g_list_append(a, b)
+#define DD_LIST_REMOVE(a, b)		\
+	a = g_list_remove(a, b)
+#define DD_LIST_FOREACH(head, elem, node)	\
+	for (elem = head; elem && ((node = elem->data) != NULL); elem = elem->next, node = NULL)
+
+typedef GList dd_list;
+static dd_list *dev_head;
+
+void add_device(const struct device_ops *dev)
+{
+	DD_LIST_APPEND(dev_head, (struct device_ops*)dev);
+}
+
+void remove_device(const struct device_ops *dev)
+{
+	DD_LIST_REMOVE(dev_head, (struct device_ops*)dev);
+}
+
+const struct device_ops *find_device(int type)
+{
+	dd_list *elem;
+	const struct device_ops *dev;
+
+	DD_LIST_FOREACH(dev_head, elem, dev) {
+		if (dev->type == type)
+			return dev;
+	}
+	return NULL;
+}
+
+void devices_init(void)
+{
+	dd_list *elem;
+	const struct device_ops *dev;
+
+	DD_LIST_FOREACH(dev_head, elem, dev) {
+		FEEDBACK_LOG("[%s] initialize", str_type[dev->type]);
+		if (dev->init)
+			dev->init();
+	}
+}
+
+void devices_exit(void)
+{
+	dd_list *elem;
+	const struct device_ops *dev;
+
+	DD_LIST_FOREACH(dev_head, elem, dev) {
+		FEEDBACK_LOG("[%s] deinitialize", str_type[dev->type]);
+		if (dev->exit)
+			dev->exit();
+	}
+}
