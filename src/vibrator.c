b517aac53b5261b31c90fc2b10e7490e25e59898
pr.jung <pr.jung@samsung.com>
2016-07-05 18:56:51 +0900

    Fix wrong type definition
    Change-Id: I158fe8d9658c95784010e2f70e8eca9881e97d7d
    Signed-off-by: pr.jung <pr.jung@samsung.com>

--- src/vibrator.c
+++ src/vibrator.c
@@ -298,7 +298,7 @@ static int vibrator_is_supported(int pattern, bool *supported)
 {
 	char *temp;
 	char *data;
-	bool ret = true;
+	int ret = 0;
 
 	if (!supported) {
 		_E("Invalid parameter : supported(NULL)"); //LCOV_EXCL_LINE
